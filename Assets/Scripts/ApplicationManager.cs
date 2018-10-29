692813ca9b66037bd468cb45a743479fc0ba198f
jamiegraham777 <jamiegraham777@gmail.com>
2018-10-29 09:11:42 -0700

    Upload project

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationManager : MonoBehaviour {
+
+   
+    public void LoadScene(string level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
---
+++ Assets/Scripts/OpenMainOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMainOnClick : MonoBehaviour {
+
+
+   
+    public void LoadScene(string level)
+    {
+        Application.LoadLevel(level);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
