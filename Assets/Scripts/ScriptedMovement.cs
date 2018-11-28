b0c251e8510a6627a541a2c59cd97b44a87c3715
Ethan <Macanasethan@gmail.com>
2018-11-28 10:34:25 -0700

    npc movement

---
+++ Assets/Scripts/ScriptedMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScriptedMovement : MonoBehaviour
+{
+	private GameObject guy;
+
+
+	public float moveSpeed = 2.0f;
+
+	private float dist;
+	CharacterController controller;
+	Animator anim;
+
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Awake(){
+		guy = GameObject.Find ("move1");
+	}
+
+	void Update()
+	{
+		dist = Vector3.Distance (guy.transform.position, transform.position);
+		if (dist > 5) {
+			anim.SetInteger ("Condition", 1);
+			transform.LookAt (guy.transform);
+
+			transform.position = Vector3.MoveTowards (transform.position, guy.transform.position, moveSpeed * Time.deltaTime);
+		} 
+		else {
+			anim.SetInteger ("Condition", 0);
+		}
+	}
+}
