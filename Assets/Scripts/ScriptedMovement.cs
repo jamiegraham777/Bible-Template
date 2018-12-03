f0797be6b37642c4024ddf704ae5280afe9be2d1
Ethan <Macanasethan@gmail.com>
2018-12-03 09:12:06 -0700

    Added second move location to david

--- Assets/Scripts/ScriptedMovement.cs
+++ Assets/Scripts/ScriptedMovement.cs
@@ -6,12 +6,14 @@ using System.Collections.Generic;
 public class ScriptedMovement : MonoBehaviour
 {
 	private GameObject guy;
+	private GameObject guy2;
 
 	public bool spot1arrived = false;
 
 	public float moveSpeed = 2.0f;
 
 	private float dist;
+	private float dist2;
 	CharacterController controller;
 	Animator anim;
 
@@ -23,13 +25,18 @@ public class ScriptedMovement : MonoBehaviour
 
 	void Awake(){
 		guy = GameObject.Find ("move1");
+		guy2 = GameObject.Find ("move2");
+	}
+
+	public IEnumerator DoTheDance() {
+		yield return new WaitForSeconds(5); // waits 3 seconds
 	}
 
 	void Update()
 	{
 		dist = Vector3.Distance (guy.transform.position, transform.position);
 		while (counter.count == 1) {
-			anim.SetInteger ("Condition", 0);
+			//anim.SetInteger ("Condition", 0);
 			if (dist > 10) {
 				anim.SetInteger ("Condition", 1);
 				transform.LookAt (guy.transform);
@@ -40,8 +47,17 @@ public class ScriptedMovement : MonoBehaviour
 				anim.SetInteger ("Condition", 3);
 			}
 		}
+		dist2 = Vector3.Distance (guy.transform.position, transform.position);
 		while (counter.count == 2) {
-			
+			if (dist2 > 10) {
+				transform.LookAt (guy2.transform);
+				anim.SetInteger ("Condition", 1);
+				transform.position = Vector3.MoveTowards (transform.position, guy2.transform.position, moveSpeed * Time.deltaTime);
+
+			} 
+			else {
+				anim.SetInteger ("Condition", 0);
+			}
 		}
 
 
